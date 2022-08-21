b2157c36aacedad23e0427c3e8b3571ab410dde6
alesso-chiavarino <alessochiavarino@gmail.com>
2022-08-21 16:07:41 -0300

    filtro casi terminado

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,74 @@
 const tbody = document.body;
 let carrito = [];
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('carrito')){
-        carrito = JSON.parse(localStorage.getItem('carrito'))
-        actualizarCarrito()
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    actualizarCarrito();
+  }
 });
 
 // declaro variables
-const botonVaciar = document.getElementById('vaciar-carrito')
-const contenedorCarrito = document.getElementById('carrito-contenedor');
-const contadorCarrito = document.getElementById('contadorCarrito');
-const precioTotal = document.getElementById('precioTotal');
+const botonVaciar = document.getElementById("vaciar-carrito");
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+const contadorCarrito = document.getElementById("contadorCarrito");
+const precioTotal = document.getElementById("precioTotal");
 
-
-botonVaciar.addEventListener('click', () => {
-    carrito.length = 0;
-    actualizarStorage();
-    actualizarCarrito();
-})
+botonVaciar.addEventListener("click", () => {
+  carrito.length = 0;
+  actualizarStorage();
+  actualizarCarrito();
+});
 
 let contenedorProductos = document.querySelector(".contenedorProductos");
 
 // renderizo las cards en el DOM
 const renderizarProductos = () => {
     productos.forEach((producto) => {
-
-        const card=document.createElement("div");
-        card.className="card card-edit col-md-3";
-        card.innerHTML=`
-            <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${producto.nombre}</h5>
-                <p class="card-text">${producto.precio}</p>
-                <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
-            </div>
+        const card = document.createElement("div");
+        card.className = "card card-edit col-md-3";
+        card.innerHTML = `
+        <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">${producto.precio}</p>
+        <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+        </div>
         `;
         contenedorProductos.appendChild(card);
-    
+        
         const boton = document.getElementById(`agregar${producto.id}`);
         
-        boton.addEventListener('click', () => {
+        boton.addEventListener("click", () => {
             agregarAlCarrito(producto.id);
         });
-        
     });
-} 
+};
 
 // funcion para pushear el producto al array carrito
 const agregarAlCarrito = (prodId) => {
-    const item = productos.find((producto) => producto.id === prodId);
-    carrito.push(item);
-    actualizarCarrito();
-    alerta();
-}
+  const item = productos.find((producto) => producto.id === prodId);
+  carrito.push(item);
+  actualizarCarrito();
+  alerta();
+};
 
 // funcion para eliminar el producto al array carrito
 const eliminarDelCarrito = (prodID) => {
-    const item = carrito.find((producto) => producto.id === prodID);
-    const indice = carrito.indexOf(item);
-    carrito.splice(indice, 1);
-    actualizarStorage();
-    actualizarCarrito();
-}
+  const item = carrito.find((producto) => producto.id === prodID);
+  const indice = carrito.indexOf(item);
+  carrito.splice(indice, 1);
+  actualizarStorage();
+  actualizarCarrito();
+};
 
 const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = '';
+  contenedorCarrito.innerHTML = "";
 
-    carrito.forEach((producto) => {
-        const cardCarrito = document.createElement('div');
-        cardCarrito.className = 'card-carrito col-md-3';
-        cardCarrito.innerHTML = `
+  carrito.forEach((producto) => {
+    const cardCarrito = document.createElement("div");
+    cardCarrito.className = "card-carrito col-md-3";
+    cardCarrito.innerHTML = `
         <div class="d-flex flex-row carrito-item justify-content-between">
             <img class="img-prod-carrito" src="${producto.imagen}" >
             <div>
@@ -81,59 +78,281 @@ const actualizarCarrito = () => {
             <button onclick ="eliminarDelCarrito(${producto.id})" class="btn btn-danger">-</button>
         </div>
         `;
-        contenedorCarrito.appendChild(cardCarrito);
-        actualizarStorage();
-    });
-    contadorCarrito.innerText = carrito.length;
-    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0);
-}
+    contenedorCarrito.appendChild(cardCarrito);
+    actualizarStorage();
+  });
+  contadorCarrito.innerText = carrito.length;
+  precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+};
 
-const actualizarStorage = () => localStorage.setItem('carrito', JSON.stringify(carrito));
-
-
+const actualizarStorage = () =>
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 
 const productosFiltrados = [];
 
 const filtrarProducto = (categoriaProd) => {
-    const categoria = productos.filter((producto) => producto.categoria === categoriaProd);
-    productosFiltrados.push(categoria);
-}
+  const categoria = productos.filter(
+    (producto) => producto.categoria === categoriaProd
+  );
+  productosFiltrados.push(categoria);
+};
 
+let inputs = document.getElementsByTagName("input");
 
-let inputs = document.getElementsByTagName('input')
-
-//PLACAS
-inputs[1].onclick = () => {
-    contenedorFiltrado.innerHTML = '';
-
-    filtrarProducto(productos[2].categoria);
-
-    padreProductos.style.display = 'none';
-    contenedorFiltrado.style.display = 'block'
-
-        productosFiltrados[0].forEach((producto) => {
-        const card = document.createElement('div')
-        card.className = 'card card-edit col-md-3'
-        card.innerHTML = `
-        <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.precio}</p>
-            <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
-        </div>
-        `
-        contenedorFiltrado.append(card);
-    })
-}
+// ------  INTENTO DE FILTRO ;)  ------ //
 
 //TODO
 inputs[0].onclick = () => {
-    padreProductos.style.display = 'flex'
-    contenedorFiltrado.style.display = 'none'
-}
+  productosFiltrados.length = 0;
+  padreProductos.style.display = "flex";
+  contenedorFiltrado.style.display = "none";
+};
+
+//PLACAS
+inputs[1].onclick = () => {
+  contenedorFiltrado.innerHTML = "";
+  productosFiltrados.length = 0;
+
+  filtrarProducto(productos[2].categoria);
+
+  padreProductos.style.display = "none";
+  contenedorFiltrado.style.display = "flex";
+  contenedorFiltrado.className =
+    "contenedorFiltrado w-75 flex-wrap justify-content-center";
+
+  productosFiltrados[0].forEach((producto) => {
+    const card = document.createElement("div");
+    card.className = "card card-edit col-md-3";
+    card.innerHTML = `
+            <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">${producto.precio}</p>
+                <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+            </div>
+            `;
+    contenedorFiltrado.append(card);
+  });
+};
+
+//PROCESADORES
+inputs[2].onclick = () => {
+  contenedorFiltrado.innerHTML = "";
+  productosFiltrados.length = 0;
+
+  filtrarProducto(productos[1].categoria);
+
+  padreProductos.style.display = "none";
+  contenedorFiltrado.style.display = "flex";
+  contenedorFiltrado.className =
+    "contenedorFiltrado w-75 flex-wrap justify-content-center";
+
+  productosFiltrados[0].forEach((producto) => {
+    const card = document.createElement("div");
+    card.className = "card card-edit col-md-3";
+    card.innerHTML = `
+            <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">${producto.precio}</p>
+                <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+            </div>
+            `;
+    contenedorFiltrado.append(card);
+  });
+};
+
+//PLACAS DE VIDEO
+inputs[3].onclick = () => {
+  contenedorFiltrado.innerHTML = "";
+  productosFiltrados.length = 0;
+
+  filtrarProducto(productos[0].categoria);
+
+  padreProductos.style.display = "none";
+  contenedorFiltrado.style.display = "flex";
+  contenedorFiltrado.className =
+    "contenedorFiltrado w-75 flex-wrap justify-content-center";
+
+  productosFiltrados[0].forEach((producto) => {
+    const card = document.createElement("div");
+    card.className = "card card-edit col-md-3";
+    card.innerHTML = `
+              <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${producto.nombre}</h5>
+                  <p class="card-text">${producto.precio}</p>
+                  <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+              </div>
+              `;
+    contenedorFiltrado.append(card);
+  });
+};
+
+//MEMORIAS RAM
+inputs[4].onclick = () => {
+  contenedorFiltrado.innerHTML = "";
+  productosFiltrados.length = 0;
+
+  filtrarProducto(productos[4].categoria);
+
+  padreProductos.style.display = "none";
+  contenedorFiltrado.style.display = "flex";
+  contenedorFiltrado.className =
+    "contenedorFiltrado w-75 flex-wrap justify-content-center";
+
+  productosFiltrados[0].forEach((producto) => {
+    const card = document.createElement("div");
+    card.className = "card card-edit col-md-3";
+    card.innerHTML = `
+                <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.precio}</p>
+                    <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+                </div>
+                `;
+    contenedorFiltrado.append(card);
+  });
+};
+
+//FUENTES DE PODER
+inputs[5].onclick = () => {
+    contenedorFiltrado.innerHTML = "";
+    productosFiltrados.length = 0;
+  
+    filtrarProducto(productos[3].categoria);
+  
+    padreProductos.style.display = "none";
+    contenedorFiltrado.style.display = "flex";
+    contenedorFiltrado.className =
+      "contenedorFiltrado w-75 flex-wrap justify-content-center";
+  
+    productosFiltrados[0].forEach((producto) => {
+      const card = document.createElement("div");
+      card.className = "card card-edit col-md-3";
+      card.innerHTML = `
+                <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.precio}</p>
+                    <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+                </div>
+                `;
+      contenedorFiltrado.append(card);
+    });
+  };
+
+//DISCOS RIGIDOS
+inputs[6].onclick = () => {
+    contenedorFiltrado.innerHTML = "";
+    productosFiltrados.length = 0;
+  
+    filtrarProducto(productos[5].categoria);
+  
+    padreProductos.style.display = "none";
+    contenedorFiltrado.style.display = "flex";
+    contenedorFiltrado.className =
+      "contenedorFiltrado w-75 flex-wrap justify-content-center";
+  
+    productosFiltrados[0].forEach((producto) => {
+      const card = document.createElement("div");
+      card.className = "card card-edit col-md-3";
+      card.innerHTML = `
+                <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.precio}</p>
+                    <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+                </div>
+                `;
+      contenedorFiltrado.append(card);
+    });
+  };
+
+//DISCOS SOLIDOS
+inputs[7].onclick = () => {
+    contenedorFiltrado.innerHTML = "";
+    productosFiltrados.length = 0;
+  
+    filtrarProducto(productos[6].categoria);
+  
+    padreProductos.style.display = "none";
+    contenedorFiltrado.style.display = "flex";
+    contenedorFiltrado.className =
+      "contenedorFiltrado w-75 flex-wrap justify-content-center";
+  
+    productosFiltrados[0].forEach((producto) => {
+      const card = document.createElement("div");
+      card.className = "card card-edit col-md-3";
+      card.innerHTML = `
+                <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.precio}</p>
+                    <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+                </div>
+                `;
+      contenedorFiltrado.append(card);
+    });
+  };
+
+//DISCOS M2
+inputs[8].onclick = () => {
+    contenedorFiltrado.innerHTML = "";
+    productosFiltrados.length = 0;
+  
+    filtrarProducto(productos[8].categoria);
+  
+    padreProductos.style.display = "none";
+    contenedorFiltrado.style.display = "flex";
+    contenedorFiltrado.className =
+      "contenedorFiltrado w-75 flex-wrap justify-content-center";
+  
+    productosFiltrados[0].forEach((producto) => {
+      const card = document.createElement("div");
+      card.className = "card card-edit col-md-3";
+      card.innerHTML = `
+                <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.precio}</p>
+                    <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+                </div>
+                `;
+      contenedorFiltrado.append(card);
+    });
+  };
+
+//COOLERS
+inputs[9].onclick = () => {
+    contenedorFiltrado.innerHTML = "";
+    productosFiltrados.length = 0;
+  
+    filtrarProducto(productos[7].categoria);
+  
+    padreProductos.style.display = "none";
+    contenedorFiltrado.style.display = "flex";
+    contenedorFiltrado.className =
+      "contenedorFiltrado w-75 flex-wrap justify-content-center";
+  
+    productosFiltrados[0].forEach((producto) => {
+      const card = document.createElement("div");
+      card.className = "card card-edit col-md-3";
+      card.innerHTML = `
+                <img src="${producto.imagen}" class="card-img-top img-producto" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.nombre}</h5>
+                    <p class="card-text">${producto.precio}</p>
+                    <button id="agregar${producto.id}" class="btn btn-dark">Agregar <i class="fas-fa-shopping-cart"</i></button>
+                </div>
+                `;
+      contenedorFiltrado.append(card);
+    });
+  };
 
 // renderizo
 renderizarProductos();
 
-const padreProductos = document.getElementById('contenedorProductos')
-const contenedorFiltrado = document.getElementById('contenedorFiltrado')
+const padreProductos = document.getElementById("contenedorProductos");
+const contenedorFiltrado = document.getElementById("contenedorFiltrado");
